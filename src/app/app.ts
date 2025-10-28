a0a6020ea2b76026d0b9783353f2476f3585ca48
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2025-10-28 16:03:49 -0500

    display navbar and toolbar
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { TabNavBar } from './tab-nav-bar/tab-nav-bar';
+import { Toolbar } from './toolbar/toolbar';
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TabNavBar, Toolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
