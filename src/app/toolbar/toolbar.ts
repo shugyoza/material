1fab5f458654aa75189136fea337a4830d9779db
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2025-10-28 16:02:44 -0500

    add toolbar.comp
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

---
+++ src/app/toolbar/toolbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-toolbar',
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule],
+  templateUrl: './toolbar.html',
+  styleUrl: './toolbar.scss',
+})
+export class Toolbar {}
