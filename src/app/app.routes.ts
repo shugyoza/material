6fe5e46e03c5cede84618baf51df013a96dddbb8
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2025-10-28 16:03:34 -0500

    update routes
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: '/login'
+    },
+    {
+        path: 'login',
+        loadComponent: () => import('../app/login/login').then(m => m.Login)
+    },
+    {
+        path: 'register',
+        loadComponent: () => import('../app/register/register').then(m => m.Register)
+    }
+];
