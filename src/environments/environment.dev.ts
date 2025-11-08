2e0beaaa50fbe222caad26e98c5303876fd6f403
Stephen Hanjaya <stephenhanjaya@gmail.com>
2025-11-08 01:40:42 -0600

    updated env implement
    Signed-off-by: Stephen Hanjaya <stephenhanjaya@gmail.com>

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -4,23 +4,7 @@ export const ENVIRONMENT = {
   name: 'dev',
   production: false,
   api: {
-    root: 'https://api.dev.harmony.csmedley.net',
+    root: 'https://api.dev.whatever.com',
     path: PATH,
-  },
-  customAuthProvider: 'AzureAD',
-  amplifyConfig: {
-    Auth: {
-      region: 'us-east-1',
-      userPoolId: 'us-east-1_RE15IrUhX',
-      userPoolWebClientId: '56lb5p74nujp8gi3gmfl7vb4u7',
-      mandatorySignIn: true,
-      oauth: {
-        domain: 'smp-auth-dev.auth.us-east-1.amazoncognito.com',
-        scope: ['email', 'openid', 'profile'],
-        redirectSignIn: 'https://dev.harmony.csmedley.net/login-callback',
-        redirectSignOut: 'https://dev.harmony.csmedley.net/logout-callback',
-        responseType: 'code', // or 'token', note that REFRESH token will only be generated when the responseType is code
-      },
-    },
   },
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,23 +5,7 @@ export const ENVIRONMENT = {
   name: 'prod',
   production: true,
   api: {
-    root: 'https://api.prod.harmony.csmedley.net',
+    root: 'https://api.prod.whatever.com',
     path: PATH,
-  },
-  customAuthProvider: 'OKTASaml',
-  amplifyConfig: {
-    Auth: {
-      region: 'us-east-1',
-      userPoolId: '',
-      userPoolWebClientId: '',
-      mandatorySignIn: true,
-      oauth: {
-        domain: '',
-        scope: ['email', 'openid', 'profile'],
-        redirectSignIn: 'https://prod.harmony.csmedley.net/login-callback',
-        redirectSignOut: 'https://prod.harmony.csmedley.net/logout-callback',
-        responseType: 'code', // or 'token', note that REFRESH token will only be generated when the responseType is code
-      },
-    },
   },
 };
