9ee578d36fce1b786075f5b313e5b836128d6794
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2025-11-11 10:09:37 -0600

    update
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { AsyncPipe, NgClass } from '@angular/common';
 
 import { Toolbar } from './core/components/toolbar/toolbar';
 import { SideMenu } from './core/components/side-menu/side-menu';
 import { SidenavDrawerComponent } from './core/components/sidenav-drawer/sidenav-drawer.component';
 import { StoreService } from './core/services/store.service/store.service';
 import { Global } from './core/services/global/global';
-import { AsyncPipe } from '@angular/common';
 
 export interface Tile {
   color: string;
@@ -19,11 +19,13 @@ export interface Tile {
   imports: [
     MatGridListModule,
     AsyncPipe,
+    NgClass,
     RouterOutlet,
     Toolbar,
     SideMenu,
     SidenavDrawerComponent,
-  ],
+    NgClass
+],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
@@ -31,11 +33,11 @@ export class App {
   readonly storeService = inject(StoreService);
   readonly global = inject(Global);
   readonly colsMap = new Map([
-    ['XSmall', 1],
-    ['Small', 1],
-    ['Medium', 2],
-    ['Large', 3],
-    ['XLarge', 3],
+    ['xs', 1],
+    ['s', 1],
+    ['m', 2],
+    ['l', 3],
+    ['xl', 3],
   ]);
 
   protected title = 'material';
--- src/app/core/services/global/global.ts
+++ src/app/core/services/global/global.ts
@@ -1,6 +1,6 @@
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { inject, Injectable } from '@angular/core';
-import { map, share } from 'rxjs';
+import { map, share, startWith } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -10,19 +10,19 @@ export class Global {
 
   // Create a map to display breakpoint names for demonstration purposes.
   private readonly _displayNameMap = new Map([
-    [Breakpoints.XSmall, 'XSmall'],
-    [Breakpoints.Small, 'Small'],
-    [Breakpoints.Medium, 'Medium'],
-    [Breakpoints.Large, 'Large'],
-    [Breakpoints.XLarge, 'XLarge'],
+    [Breakpoints.XSmall, 'xs'],
+    [Breakpoints.Small, 's'],
+    [Breakpoints.Medium, 'm'],
+    [Breakpoints.Large, 'l'],
+    [Breakpoints.XLarge, 'xl'],
   ]);
 
   private readonly _gridColMap = new Map([
-    ['XSmall', 4],
-    ['Small', 4],
-    ['Medium', 8],
-    ['Large', 12],
-    ['XLarge', 12],
+    ['xs', 4],
+    ['s', 4],
+    ['m', 8],
+    ['l', 12],
+    ['xl', 12],
   ]);
 
   readonly breakpoint$ = this._breakpointObserver
@@ -34,11 +34,23 @@ export class Global {
       Breakpoints.XLarge,
     ])
     .pipe(
+      startWith({
+        "matches": true,
+        "breakpoints": {
+          "(max-width: 599.98px)": false,
+          "(min-width: 600px) and (max-width: 959.98px)": false,
+          "(min-width: 960px) and (max-width: 1279.98px)": false,
+          "(min-width: 1280px) and (max-width: 1919.98px)": true,
+          "(min-width: 1920px)": false
+        }
+      }),
       map(value => {
         const breakpoints = Object.entries(value.breakpoints);
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const [width] = breakpoints
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           .filter(([_, value]) => value === true)
+          // eslint-disable-next-line @typescript-eslint/no-unused-vars
           .map(([key, _]) => key);
         const breakpoint = this._displayNameMap.get(width) ?? 'Unknown';
 
