7a90b791b57c9b11574980a7886e831f46233249
Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>
2025-11-13 13:44:09 -0600

    added localStorage service
    Signed-off-by: Stephen Hanjaya <stephen.hanjaya@sonymusicpub.com>

---
+++ src/app/core/services/local-storage/local-storage.ts
@@ -0,0 +1,51 @@
+import { Injectable, inject } from '@angular/core';
+import { DOCUMENT } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorage {
+  private readonly localStorage = inject(DOCUMENT).defaultView?.localStorage;
+
+  get<T>(key: string): T | null {
+    const value = this.localStorage?.getItem(key);
+
+    if (!value) {
+      return null;
+    }
+
+    const isValidJSON = this.isValidJSON(value);
+
+    if (!isValidJSON) {
+      return null;
+    }
+
+    return JSON.parse(value);
+  }
+
+  set(key: string, value: unknown): void {
+    this.localStorage?.setItem(key, JSON.stringify(value));
+  }
+
+  remove(key: string): void {
+    this.localStorage?.removeItem(key);
+  }
+
+  removeKeys(keys: string[]): void {
+    keys.forEach((key) => this.remove(key));
+  }
+
+  clear(): void {
+    this.localStorage?.clear();
+  }
+
+  private isValidJSON(value: string): boolean {
+    try {
+      JSON.parse(value);
+
+      return true;
+    } catch {
+      return false;
+    }
+  }
+}
