c9b2f77e0cec2a2f172cb3c2f22f26cc5cb4d154
Stephen Hanjaya <stephenhanjaya@gmail.com>
2026-03-20 08:57:06 -0700

    type guards object
    Signed-off-by: Stephen Hanjaya <stephenhanjaya@gmail.com>

---
+++ src/app/shared/library/type-guards/is-object.ts
@@ -0,0 +1,21 @@
+import { isNumber } from './is-number';
+import { isString } from './is-string';
+
+export function isPlainObject(value: unknown): value is object {
+	const not = {
+		null: value !== null,
+		array: !Array.isArray(value),
+		string: !isString(value),
+		number: !isNumber(value),
+		date: !(value instanceof Date),
+	};
+
+	return (
+		not.null &&
+		not.array &&
+		not.string &&
+		not.number &&
+		not.date &&
+		typeof value === 'object'
+	);
+}
