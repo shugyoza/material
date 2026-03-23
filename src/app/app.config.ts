b9acb6e37b949046e5c474a4c26ece310d5cbc89
Stephen Hanjaya <stephenhanjaya@gmail.com>
2026-03-22 17:32:55 -0700

    updated app.config
    Signed-off-by: Stephen Hanjaya <stephenhanjaya@gmail.com>

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,11 +4,12 @@ import {
 	provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { provideHttpClient } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { StoreService } from './core/services/store.service/store.service';
 import { Global } from './core/services/global/global';
-import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
 	providers: [
@@ -18,5 +19,9 @@ export const appConfig: ApplicationConfig = {
 		StoreService,
 		Global,
 		provideHttpClient(),
+		{
+			provide: MAT_DIALOG_DEFAULT_OPTIONS,
+			useValue: { hasBackdrop: true },
+		},
 	],
 };
